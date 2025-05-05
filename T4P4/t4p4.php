7721282131e5fdacf961f17e538452f182aad1aa
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2025-05-05 09:56:18 -0500

    menu arreglado

--- T4P4/t4p4.php
+++ T4P4/t4p4.php
@@ -11,17 +11,20 @@
 
 <body>
 
-    <!-- Inclusión del menú -->
-    <?php include("../base/menu.html"); ?>
+    <div class="container-fluid"><!-- Contenedor principal de la página -->
+        <!-- Inclusión del menú -->
+        <?php include("../base/menu.html"); ?>
 
-    <div class="card h-100"><!--Titulo de la aplicación-->
-        <h1 class="card-header">Cálculo trigonométrico de un ángulo</h1>
-    </div><!--Titulo de la aplicación-->
-    <!-- Inclusión del formulario para ingresar el ángulo -->
-    <?php include("form4.php"); ?>
+        <div class="card h-100"><!--Titulo de la aplicación-->
+            <h1 class="card-header">Cálculo trigonométrico de un ángulo</h1>
+        </div><!--Titulo de la aplicación-->
+        <!-- Inclusión del formulario para ingresar el ángulo -->
+        <?php include("form4.php"); ?>
 
-    <!-- Inclusión del archivo que contiene los cálculos y resultados -->
-    <?php include("calculos.php"); ?>
+        <!-- Inclusión del archivo que contiene los cálculos y resultados -->
+        <?php include("calculos.php"); ?>
+
+    </div>
 
     <!-- Inclusión del pie de página -->
     <?php include("../base/footer.html") ?>
